0aea4a4434eb4a4d366fc5d51eeed6d92ba5a3d5
Hajo Wolfram <hajowolfram@dhcp-10-105-84-228.wireless.northwestern.private>
2025-07-02 13:09:52 -0500

    fixed port test

--- port_test.py
+++ port_test.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATA_PORT = os.getenv('DATA_PORT')
-PORT = "/dev/cu.usbmodemR00810384" # CHANGE
 BAUDRATE = 921600
 
 try:
-    ser = serial.Serial(PORT, BAUDRATE, timeout=1)
-    print(f"Listening on {PORT} at {BAUDRATE} baud...\nPress Ctrl+C to stop.")
+    ser = serial.Serial(DATA_PORT, BAUDRATE, timeout=1)
+    print(f"Listening on {DATA_PORT} at {BAUDRATE} baud...\nPress Ctrl+C to stop.")
     
     while True:
         data = ser.read(100)  # Read up to 100 bytes at a time
